954b97778510f96ac16c7c9e061776dbe5bb456b
Luiz Paulo <luizpaulorodrigues2005@gmail.com>
2023-02-08 00:00:17 -0300

    CORS

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { prisma } from "./lib/prisma";
 
 const app = express();
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 // Rotas
 
